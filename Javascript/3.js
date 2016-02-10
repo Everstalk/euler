30621052b84b09499c5d000ae6279a4aac528726
Jorge Gonzalez <jrgqhd@gmail.com>
2016-02-09 21:18:29 -0500

    Update 3.js

--- Javascript/3.js
+++ Javascript/3.js
@@ -1,16 +1,16 @@
 // What is the largest prime factor of the number 600851475143?
-    function largest_prime(num) {
-        var factors = [];
-        var p = 2;
+function largest_prime(num) {
+    var factors = [];
+    var p = 2;
 
-        while (num > 1) {
-            while (num % p === 0) {
-                factors.push(p);
-                num /= p;
-            }
-
-            p += 1;
+    while (num > 1) {
+        while (num % p === 0) {
+            factors.push(p);
+            num /= p;
         }
 
-        return Math.max(...factors);
+        p += 1;
     }
+
+    return Math.max(...factors);
+}
